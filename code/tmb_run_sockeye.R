66883c247ac43aebed41331ce1b8d9282cc8bb1d
Catarina Wor <Catarina Wor>
2022-07-08 13:35:27 -0700

    i have no idea what I changed

--- code/tmb_run_sockeye.R
+++ code/tmb_run_sockeye.R
@@ -3,10 +3,14 @@
 library(here)
 library(TMB)
 
-sock_dat<- read.csv(here('data','filtered datasets','sockeye_final.csv'))
-sock_info<- read.csv(here('data','sockeye','sockeye_info.csv'))
+sock_dat<- read.csv(here('data','filtered datasets','salmon_productivity_compilation_jun2022.csv'))
+sock_info<- read.csv(here('data','filtered datasets','all_stocks_info_jun2022.csv'))
 
-sock_info<- subset(sock_info, Stock.ID %in% sock_dat$stock.id)
+head(sock_dat)
+head(sock_info)
+
+
+sock_info<- subset(sock_info, stock.id %in% sock_dat$stock.id)
 
 #compile and load TMB models
 #Model 1 static model
@@ -19,12 +23,12 @@ compile("TMBmodels/Ricker_tva_Smax.cpp")
 dyn.load(dynlib("TMBmodels/Ricker_tva_Smax"))
 
 #Model 3 tv a and static Srep (vary b)
-compile("TMBmodels/Ricker_tva_Srep.cpp")
-dyn.load(dynlib("TMBmodels/Ricker_tva_Srep"))
+#compile("TMBmodels/Ricker_tva_Srep.cpp")
+#dyn.load(dynlib("TMBmodels/Ricker_tva_Srep"))
 
 #Model 4 tv  b
-compile("TMBmodels/Ricker_tvb.cpp")
-dyn.load(dynlib("TMBmodels/Ricker_tvb"))
+#compile("TMBmodels/Ricker_tvb.cpp")
+#dyn.load(dynlib("TMBmodels/Ricker_tvb"))
 
 #Model 4.2 tv  b
 compile("TMBmodels/Ricker_tvlogb.cpp")
@@ -39,29 +43,39 @@ dyn.load(dynlib("TMBmodels/Ricker_tva_tvb"))
 #dyn.load(dynlib("TMBmodels/Ricker_tvbdois"))
 
 SmaxAIC<-NULL
-SrepAIC<-NULL
+#SrepAIC<-NULL
 simpleAIC<-NULL
-bvaryAIC<-NULL
+#bvaryAIC<-NULL
 logbvaryAIC<-NULL
 abvaryAIC<-NULL
 
 
 for(i in seq_len(nrow(sock_info))){
-  s <- subset(sock_dat,stock.id==sock_info$Stock.ID[i])
+  #i<-2
+  s <- subset(sock_dat,stock.id==sock_info$stock.id[i])
+  s$spawners[s$spawners==0]<-NA
 
-  srm <- lm(s$logR_S~ s$spawners)
+  s$spawnersavg <- s$spawners#/1000# mean(s$spawners,na.rm=T)
+  s$logR_S <- log(s$recruits/s$spawnersavg)
+  #SRdata<-list(obs_logRS=s$logR_S,obs_S=s$spawnersavg)
   
-  SRdata<-list(obs_logRS=s$logR_S,obs_S=s$spawners)
+
+  #s$spawnersavg <- s$spawners/mean(s$spawners,na.rm=T)
+  #s$logR_S <- log(s$recruits/s$spawnersavg)
+
+  srm <- lm(s$logR_S~ s$spawnersavg, na.action=na.omit)
+  
+  SRdata<-list(obs_logRS=s$logR_S,obs_S=s$spawnersavg)
   
   #Model 1 - static a & b
-  #to be implemented
   parameters_simple<- list(
-    alpha=srm$coefficients[1],
-    logbeta = log(ifelse(-srm$coefficients[2]<0,1e-08,-srm$coefficients[2])),
+    alpha=srm$coefficients[[1]],
+    logbeta = log(ifelse(-srm$coefficients[[2]]<0,1e-08,-srm$coefficients[[2]])),
     logsigobs=log(.4)
     )
 
   obj_simple <- MakeADFun(SRdata,parameters_simple,DLL="Ricker_simple")
+  
   newtonOption(obj_simple, smartsearch=FALSE)
 
   opt_simple <- nlminb(obj_simple$par,obj_simple$fn,obj_simple$gr)
@@ -94,28 +108,28 @@ for(i in seq_len(nrow(sock_info))){
   SmaxAIC[i]<-2*4-2*-opt$objective
 
   #Model 3 tv a and static Srep (vary b)
-  parametersSrep<- list(
-    alphao=srm$coefficients[1],
-    logSrep = log(ifelse(srm$coefficients[1]/-srm$coefficients[2]<0,10000,srm$coefficients[1]/-srm$coefficients[2])),
-    #rho=.2,
-    #logvarphi= 0.1,
-    logsigobs=log(.4),
-    logsiga=log(.4),
-    alpha=rep(srm$coefficients[1],length(s$recruits))
-    )
+  #parametersSrep<- list(
+  #  alphao=srm$coefficients[1],
+  #  logSrep = log(ifelse(srm$coefficients[1]/-srm$coefficients[2]<0,10000,srm$coefficients[1]/-srm$coefficients[2])),
+  #  #rho=.2,
+  #  #logvarphi= 0.1,
+  #  logsigobs=log(.4),
+  #  logsiga=log(.4),
+  #  alpha=rep(srm$coefficients[1],length(s$recruits))
+  #  )
+  #
+  #
+  #objSrep <- MakeADFun(SRdata,parametersSrep,DLL="Ricker_tva_Srep",random="alpha")
+  #newtonOption(objSrep, smartsearch=FALSE)
+  ##objSrep$fn()
+  ##objSrep$gr()
+  #optSrep <- nlminb(objSrep$par,objSrep$fn,objSrep$gr)
+  ##repSrep <- objSrep$report()
 
+  #SrepAIC[i]<-2*4-2*-optSrep$objective
 
-  objSrep <- MakeADFun(SRdata,parametersSrep,DLL="Ricker_tva_Srep",random="alpha")
-  newtonOption(objSrep, smartsearch=FALSE)
-  #objSrep$fn()
-  #objSrep$gr()
-  optSrep <- nlminb(objSrep$par,objSrep$fn,objSrep$gr)
-  #repSrep <- objSrep$report()
-
-  SrepAIC[i]<-2*4-2*-optSrep$objective
-
-  #Model 4.2 tv logb 
-
+  #Model 4.2 tv logb
+ 
   parametersb<- list(
     logbetao = log(ifelse(-srm$coefficients[[2]]<0,1e-08,-srm$coefficients[[2]])),
     alpha=srm$coefficients[[1]],
@@ -149,43 +163,42 @@ for(i in seq_len(nrow(sock_info))){
 #dyn.load(dynlib("TMBmodels/Ricker_tvb"))
 
 
-  parametersb<- list(
-    logbetao = log(ifelse(-srm$coefficients[2]<0,1e-08,-srm$coefficients[2])),
-    alpha=srm$coefficients[1],
-    logsigobs=log(.4),
-    logsigb=log(.4),
-    #rho=.4,
-    #logvarphi= 0.5,
-    logbeta=(rep(-srm$coefficients[2],length(s$spawners)))
-    )
-
-  objb <- MakeADFun(SRdata,parametersb,DLL="Ricker_tvb",random="logbeta",lower=c(-10,-20,-6,-6),
-               upper=c(10,0,2,2))
-  newtonOption(objb, smartsearch=FALSE)
-#  objb$env$inner.control$tol10 <- 0
-  objb$fn()
-  objb$gr()
+  #parametersb<- list(
+  #  logbetao = log(ifelse(-srm$coefficients[2]<0,1e-08,-srm$coefficients[2])),
+  #  alpha=srm$coefficients[1],
+  #  logsigobs=log(.4),
+  #  logsigb=log(.4),
+  #  #rho=.4,
+  #  #logvarphi= 0.5,
+  #  logbeta=(rep(-srm$coefficients[2],length(s$spawners)))
+  #  )
+  #
+  #objb <- MakeADFun(SRdata,parametersb,DLL="Ricker_tvb",random="logbeta",lower=c(-10,-20,-6,-6),
+  #             upper=c(10,0,2,2))
+  #newtonOption(objb, smartsearch=FALSE)
+  ##  objb$env$inner.control$tol10 <- 0
+  #objb$fn()
+  #objb$gr()
   #optb <- nlminb(objb$par,objb$fn,objb$gr)
-
-  skip_to_next<-FALSE
-  tryCatch(
-    {optb <- nlminb(objb$par,objb$fn,objb$gr)},
-    error =function(e){ skip_to_next <<- TRUE}) 
-  
- 
-  if(skip_to_next) { 
-    bvaryAIC[i]<-NA
-    next 
-  }else{
-    bvaryAIC[i]<-2*4-2*-optb$objective
-  }   
+  #
+  #skip_to_next<-FALSE
+  #tryCatch(
+  #  {optb <- nlminb(objb$par,objb$fn,objb$gr)},
+  #  error =function(e){ skip_to_next <<- TRUE}) 
+  #
+  #if(skip_to_next) { 
+  #  bvaryAIC[i]<-NA
+  #  next 
+  #}else{
+  #  bvaryAIC[i]<-2*4-2*-optb$objective
+  #}   
 
 
   #Model 5 tv a and b 
   #compile("TMBmodels/Ricker_tva_tvb.cpp")
   #dyn.load(dynlib("TMBmodels/Ricker_tva_tvb"))
 
-  print(paste("i is ", i))
+  
   parametersab<- list(
     logbetao = log(ifelse(-srm$coefficients[[2]]<0,1e-08,-srm$coefficients[[2]])),
     alphao=srm$coefficients[[1]],
@@ -216,19 +229,30 @@ for(i in seq_len(nrow(sock_info))){
 }
 
 
-
-deltaSmaxAIC<-SmaxAIC-pmin(SmaxAIC,SrepAIC,simpleAIC,bvaryAIC, logbvaryAIC,abvaryAIC, na.rm=T)
-deltaSrepAIC<-SrepAIC-pmin(SmaxAIC,SrepAIC,simpleAIC,bvaryAIC,logbvaryAIC,abvaryAIC, na.rm=T)
-deltasimpleAIC<-simpleAIC-pmin(SmaxAIC,SrepAIC,simpleAIC,bvaryAIC,logbvaryAIC,abvaryAIC, na.rm=T)
-deltabAIC<-bvaryAIC-pmin(SmaxAIC,SrepAIC,simpleAIC,bvaryAIC,logbvaryAIC,abvaryAIC, na.rm=T)
-deltalogbAIC<-logbvaryAIC-pmin(SmaxAIC,SrepAIC,simpleAIC,bvaryAIC,logbvaryAIC,abvaryAIC, na.rm=T)
-deltaabAIC<-abvaryAIC-pmin(SmaxAIC,SrepAIC,simpleAIC,bvaryAIC,logbvaryAIC,abvaryAIC, na.rm=T)
+deltaSmaxAIC<-SmaxAIC-pmin(SmaxAIC,simpleAIC, logbvaryAIC,abvaryAIC, na.rm=T)
+deltasimpleAIC<-simpleAIC-pmin(SmaxAIC,simpleAIC, logbvaryAIC,abvaryAIC, na.rm=T)
+deltalogbAIC<-logbvaryAIC-pmin(SmaxAIC,simpleAIC, logbvaryAIC,abvaryAIC, na.rm=T)
+deltaabAIC<-abvaryAIC-pmin(SmaxAIC,simpleAIC, logbvaryAIC,abvaryAIC, na.rm=T)
 
 sum(!is.na(deltasimpleAIC)&deltasimpleAIC==0)/length(deltasimpleAIC)
 sum(!is.na(deltaSmaxAIC)&deltaSmaxAIC==0)/length(deltaSmaxAIC)
-sum(!is.na(deltaSrepAIC)&deltaSrepAIC==0)/length(deltaSrepAIC)
-sum(!is.na(deltabAIC)&deltabAIC==0)/length(deltabAIC)
+sum(!is.na(deltalogbAIC)&deltabAIC==0)/length(deltabAIC)
 sum(!is.na(deltaabAIC)&deltaabAIC==0)/length(deltaabAIC)
+
+
+
+#deltaSmaxAIC<-SmaxAIC-pmin(SmaxAIC,SrepAIC,simpleAIC,bvaryAIC, logbvaryAIC,abvaryAIC, na.rm=T)
+#deltaSrepAIC<-SrepAIC-pmin(SmaxAIC,SrepAIC,simpleAIC,bvaryAIC,logbvaryAIC,abvaryAIC, na.rm=T)
+#deltasimpleAIC<-simpleAIC-pmin(SmaxAIC,SrepAIC,simpleAIC,bvaryAIC,logbvaryAIC,abvaryAIC, na.rm=T)
+#deltabAIC<-bvaryAIC-pmin(SmaxAIC,SrepAIC,simpleAIC,bvaryAIC,logbvaryAIC,abvaryAIC, na.rm=T)
+#deltalogbAIC<-logbvaryAIC-pmin(SmaxAIC,SrepAIC,simpleAIC,bvaryAIC,logbvaryAIC,abvaryAIC, na.rm=T)
+#deltaabAIC<-abvaryAIC-pmin(SmaxAIC,SrepAIC,simpleAIC,bvaryAIC,logbvaryAIC,abvaryAIC, na.rm=T)
+
+#sum(!is.na(deltasimpleAIC)&deltasimpleAIC==0)/length(deltasimpleAIC)
+#sum(!is.na(deltaSmaxAIC)&deltaSmaxAIC==0)/length(deltaSmaxAIC)
+#sum(!is.na(deltaSrepAIC)&deltaSrepAIC==0)/length(deltaSrepAIC)
+#sum(!is.na(deltabAIC)&deltabAIC==0)/length(deltabAIC)
+#sum(!is.na(deltaabAIC)&deltaabAIC==0)/length(deltaabAIC)
 
 
 
